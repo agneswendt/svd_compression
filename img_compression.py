deb6443dd1eb3c429bd8f421c10341c3640c51ac
Agnes Wendt <agnes@wendt.nu>
2021-04-17 22:34:56 +0200

    works

--- img_compression.py
+++ img_compression.py
@@ -4,22 +4,57 @@ import matplotlib.pyplot as plt
 import sys
 
 def main(f_name):
-    img = Image.open(f_name)
-    img_gray = img.convert("LA")
-    imgmat = np.array(list(img_gray.getdata(band=0)), float)
-    imgmat.shape = (img_gray.size[1], img_gray.size[0])
-    imgmat = np.matrix(imgmat)
+    img = Image.open(f_name).convert('RGB')
+    red, green, blue = img.split()
+
+    red_img = np.array(red)
+    red_img.shape = (red.size[1], red.size[0])
+
+    green_img = np.array(green)
+    green_img.shape = (green.size[1], green.size[0])
+
+    blue_img = np.array(blue)
+    blue_img.shape = (blue.size[1], blue.size[0])
+
+    print(red_img, blue_img, green_img)
+
     plt.figure(figsize=(9,6))
-    plt.imshow(imgmat, cmap='gray');
-    plt.title(f"original, matrix rank {np.linalg.matrix_rank(imgmat)}")
+    rgb = to_matrix(red_img, green_img, blue_img, red.size[0], red.size[1])
+
+    print()
+
+    plt.imshow(rgb)
+    plt.title(f"original, matrix rank {np.linalg.matrix_rank(red_img)}")
     plt.show()
-    U, sigma, V = np.linalg.svd(imgmat)
-    for i in [55, 20, 100, 500]:
-        reconstimg = np.matrix(U[:, :i]) * np.diag(sigma[:i]) * np.matrix(V[:i, :])
-        plt.imshow(reconstimg, cmap='gray')
-        title = f"rank {np.linalg.matrix_rank(reconstimg)}"
+
+    svd_r = np.linalg.svd(red_img)
+    svd_g = np.linalg.svd(green_img)
+    svd_b = np.linalg.svd(blue_img)
+
+    for i in [5, 20, 100, 500]:
+        reconst = to_matrix(compress(svd_r, i), compress(svd_g, i),
+                            compress(svd_b, i), red.size[0], red.size[1])
+        plt.imshow(reconst)
+        title = f"rank {np.linalg.matrix_rank(compress(svd_r, i))}"
         plt.title(title)
         plt.show()
+
+
+def to_matrix(r, g, b, w, h):
+    res = []
+    for y in range(h):
+        row = []
+        for x in range(w):
+            if r[y, x]/255 > 1: print(r[y,x])
+            row.append((r[y, x]/255, g[y, x]/255, b[y, x]/255))
+        res.append(row)
+    return np.array(res)
+
+
+def compress(svd, rank):
+    U, sigma, V = svd
+    return np.matrix(U[:, :rank]) * np.diag(sigma[:rank]) * np.matrix(V[:rank, :])
+
 
 
 if __name__=="__main__":
